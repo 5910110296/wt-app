fc9c6442968d9363ca2b2a0803e4fb8d27721933
5910110296 <5910110296@psu.ac.th>
2018-10-01 10:18:09 +0700

    reactnative

--- components/Forecast.js
+++ components/Forecast.js
@@ -1,16 +1,38 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
 
+export default class Forcast extends React.Component {
 
-export default class Forecast extends React.Component{
-    render(){
-        return (
-            <View>
-                <Text>{this.props.main}</Text>
-                <Text>{this.props.description}</Text>
-                <Text>{this.props.temp}</Text>
-                <Text>°C</Text>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+          <Text style={styles.main}>{this.props.main}</Text>
+          <Text style={styles.des}>{this.props.description}</Text>
+          <Text style={styles.temp}>{this.props.temp} °C</Text>
+      </View>
+    );
+  }
 }
+const styles = StyleSheet.create({
+    container: { 
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+    main:{
+        fontSize: 35 ,
+        paddingTop: 10,
+        color : 'white',
+    },
+    des:{
+      fontSize: 15 ,
+      paddingTop: 25,
+      color : 'white',
+    },
+    temp:{
+      fontSize: 30 ,
+      paddingTop: 30,
+      paddingBottom:25,
+      color : 'white',
+    },
+});  
--- components/Weather.js
+++ components/Weather.js
@@ -1,29 +1,81 @@
 import React from 'react';
-import { StyleSheet, Text, View ,ImageBackground } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground } from 'react-native';
 import Forecast from './Forecast';
 
 export default class Weather extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      forecast: {
-        main: '-', description: '-',temp: 0
+    constructor(props) {
+      super(props);
+      this.state = {
+        forecast: {
+          main: 'main', description: 'description', temp: 0
+        }
       }
     }
-  }
-  render() {
-    return (
-      <View style={styles.container}>
+    fetchData = () => {
+      fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${
+          this.props.zipCode
+        },th&units=metric&AppID=fd68c0f2039c5a25f666a9ff374bc93e`
+      )
+        .then(response => response.json())
+        .then(json => {
+          this.setState({
+            forecast: {
+              main: json.weather[0].main,
+              description: json.weather[0].description,
+              temp: json.main.temp
+            }
+          });
+        })
+        .catch(error => {
+          console.warn(error);
+        });
+    };
+    componentDidMount = () => this.fetchData();
+    componentDidUpdate = (prevProps) => {
+      if(prevProps.zipCode !== this.props.zipCode){
+        this.fetchData()
+      }
+    }
+  
+    render() {
+        return (
+        <View style={styles.container}>
+        
         <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
-          <Text>Zip code is {this.props.zipCode}.</Text>
-          <Forecast{...this.state.forecast}/>
-        </ImageBackground>
-      </View>
-    );
+        
+          <View style={styles.layout }>
+       
+          <Text    style={styles.red } > Zip code is {this.props.zipCode}.</Text>
+          <Forecast {...this.state.forecast} />
+          </View>
+          </ImageBackground>
+        </View>
+        );
+      }
   }
-}
-
-const styles = StyleSheet.create({
-  container: { paddingTop: 25},
-  backdrop: { width: '100%', height: '100%'},
-});
+   const styles = StyleSheet.create({
+     container: { 
+        paddingTop: 25,  
+     },
+     layout: {
+      
+        flexDirection:'column',
+        alignItems: 'center',
+       
+        justifyContent:'space-between',
+        height: 300,  opacity: 0.4, 
+        backgroundColor: 'black',
+     },
+    red : {
+      
+        paddingTop: 25,
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 25,
+    },
+    backdrop: { width: '100%', 
+    height: '100%',
+    },
+   });
